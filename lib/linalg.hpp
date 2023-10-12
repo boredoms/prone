695cae138a6823c5b13ef9a01eaa08ce193d8c04
voetschm <maximilian.voetsch@univie.ac.at>
2023-10-12 19:27:32 +0200

    add iterator based squared euclidean distance

--- lib/linalg.hpp
+++ lib/linalg.hpp
@@ -1,6 +1,8 @@
 #ifndef LINALG_H_
 #define LINALG_H_
 
+#include <vector>
+
 /**
  * Calculate the squared euclidean distance of two datapoints in the dataset X
  *
@@ -18,6 +20,37 @@ double squared_euclidean_distance(int d, int i, int j, double const *X) {
   }
 
   return distance;
+}
+
+template <typename it, typename it_>
+double squared_euclidean_distance(it begin, it end, it_ iter_) {
+  double distance = 0.0;
+
+  for (it iter = begin; iter != end; iter++, iter_++) {
+    distance += std::pow(*iter - *iter_, 2);
+  }
+
+  return distance;
+}
+
+/**
+ * Calculate the mean of each feature in the dataset.
+ *
+ */
+std::vector<double> column_means(int n, int d, double const *X) {
+  std::vector<double> means(d, 0.0);
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < d; j++) {
+      means[j] += X[i * d + j];
+    }
+  }
+
+  for (int i = 0; i < d; i++) {
+    means[i] /= n;
+  }
+
+  return means;
 }
 
 #endif // LINALG_H_
