3f54276fdf776e2d3d5955638d0960c1f7f5b47d
voetschm <maximilian.voetsch@univie.ac.at>
2024-01-05 14:03:48 +0100

    change path

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import numpy as np
 from prone import prone
 import time
 
-X = np.genfromtxt('experiments/datasets/bio_train.dat', delimiter=',')
+X = np.genfromtxt('datasets/bio_train.dat', delimiter=',')
 k = 1000
 
 start = time.time()
